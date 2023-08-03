3e805bae29118ee21f57559f8675ad88582b7ac1
Maymonakh <maymonakhanfar@gmail.com>
2023-08-03 03:15:17 +0300

    Add animations and Fade

--- assets/JS/index.js
+++ assets/JS/index.js
@@ -4,3 +4,17 @@ const navMenu = document.querySelector('aside');
 menuIcon.addEventListener('click', () => {
   navMenu.classList.toggle('show');
 });
+
+let elementsArray = document.querySelectorAll(".section_wrapper");
+window.addEventListener('scroll', fadeIn ); 
+function fadeIn() {
+    for (var i = 0; i < elementsArray.length; i++) {
+        var elem = elementsArray[i]
+        var distInView = elem.getBoundingClientRect().top - window.innerHeight + 20;
+        if (distInView < 0) {
+            elem.classList.add("inView");
+        } else {
+            elem.classList.remove("inView");
+        }
+    }
+}
